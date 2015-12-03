19942b260d240c32ca93f1876085514a9aa52362
XGHeaven <619943612@qq.com>
2015-12-03 22:14:23 +0800

    fix star error in dialog

--- src/js/controller/dialog.js
+++ src/js/controller/dialog.js
@@ -7,16 +7,20 @@ cnode.controller('TopicDialogController', ['$scope', '$mdDialog', 'API', 'TopicI
 		$state.go('main.topic', {id})
 		$scope.close()
 	}
+
+	$scope.id = id
+
+	$scope.$on('TopicTitle', (e, title) => $scope.title = title)
 	
-	API.getTopic(id).success(data => {
-		data.data.content = md.render(data.data.content)
+	// API.getTopic(id).success(data => {
+	// 	data.data.content = md.render(data.data.content)
 		
-		data.data.replies.forEach(reply => {
-			reply.content = md.render(reply.content)
-		})
+	// 	data.data.replies.forEach(reply => {
+	// 		reply.content = md.render(reply.content)
+	// 	})
 		
-		angular.extend($scope, data.data)
-	})
+	// 	angular.extend($scope, data.data)
+	// })
 	
 	$scope.close = () => {
 		$mdDialog.hide()
--- src/js/controller/topic.js
+++ src/js/controller/topic.js
@@ -3,12 +3,15 @@ import md from '../utils/md'
 import angular from 'angular'
 
 cnode.controller('TopicController', ['$scope', 'API', '$state', 'User', ($scope, API, $state, User) => {
-	API.getTopic($state.params.id).success(data => {
+	// use in dialog and router
+	API.getTopic($state.params.id || $scope.id).success(data => {
 		data.data.content = md.render(data.data.content)
 		
 		data.data.replies.forEach(reply => {
 			reply.content = md.render(reply.content)
 		})
+
+		$scope.$emit('TopicTitle', data.data.title)
 
 		angular.extend($scope, data.data)
 	})
