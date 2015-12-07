5bfc62e9c0d6981b04b73e6082c51108d83c1612
XGHeaven <619943612@qq.com>
2015-12-07 08:49:09 +0800

    fix setting accesstoken

--- src/js/controller/setting.js
+++ src/js/controller/setting.js
@@ -1,7 +1,7 @@
 import cnode from '../cnode'
 
 cnode.controller('SettingController', ['$scope', '$mdToast', 'User', ($scope, $mdToast, User) => {
-	$scope.setting = User.getSetting() || {}
+	$scope.setting = angular.extend({}, User.getSetting() || {})
 	$scope.save = () => {
 		// $mdToast.show($mdToast.simple().content('Saving...').position('top right').hideDelay(2000))
 		User.saveSetting($scope.setting)
