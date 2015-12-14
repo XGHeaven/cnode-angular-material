9ff90605186ea1a31dedc84d224f6aac0a110190
XGHeaven <619943612@qq.com>
2015-12-14 23:25:21 +0800

    add refresh button

--- src/js/controller/main.js
+++ src/js/controller/main.js
@@ -1,8 +1,10 @@
 import cnode from '../cnode'
 
-cnode.controller('MainController', ['$scope', ($scope) => {
+cnode.controller('MainController', ƒ(($scope, $state) => {
 	// set title
 	$scope.$on('TopicTitle', (e, title) => $scope.mainTitle = title)
 	// when router changed, reset title
 	$scope.$on('$stateChangeStart', (e) => $scope.mainTitle = '')
-}])
+
+	$scope.refresh = ::$state.reload
+}))
