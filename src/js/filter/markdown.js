495909394dd688af27f3b0dbfa8c659c665481e6
XGHeaven <619943612@qq.com>
2015-12-16 15:26:19 +0800

    add transform user info link in content

--- src/js/filter/markdown.js
+++ src/js/filter/markdown.js
@@ -1,7 +1,13 @@
 import cnode from '../cnode'
 
-cnode.filter('Markdown', ['Marked', (Marked) => {
+cnode.filter('Markdown', ƒ((Marked) => {
 	return (string) => {
+		if (!string) return ''
+
+		string = string.replace(/\[@(\w+)\]\(\/user\/\1\)/g, (match, name) => {
+			return `[@${name}](#/main/user/${name}) `
+		})
+
 		return Marked.render(string)
 	}
-}])
+}))
