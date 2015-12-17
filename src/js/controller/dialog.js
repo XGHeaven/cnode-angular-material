7edf3b8619cf35347683e67296116087bf3e4a78
XGHeaven <619943612@qq.com>
2015-12-18 00:34:47 +0800

    fix click user link in dialog window doesn't close dialog

--- src/js/controller/dialog.js
+++ src/js/controller/dialog.js
@@ -1,27 +1,24 @@
 import cnode from '../cnode'
 import angular from 'angular'
 
-cnode.controller('TopicDialogController', ['$scope', '$mdDialog', 'API', 'TopicID', '$state', ($scope, $mdDialog, API, id, $state) => {
+cnode.controller('TopicDialogController', ƒ(($scope, $mdDialog, API, TopicID, $state) => {
+	// for convinence
+	let id = TopicID
+
 	$scope.gotoTopic = () => {
 		$state.go('main.topic', {id})
-		$scope.close()
 	}
 
 	$scope.id = id
 
 	$scope.$on('TopicTitle', (e, title) => $scope.title = title)
-	
-	// API.getTopic(id).success(data => {
-	// 	data.data.content = md.render(data.data.content)
-		
-	// 	data.data.replies.forEach(reply => {
-	// 		reply.content = md.render(reply.content)
-	// 	})
-		
-	// 	angular.extend($scope, data.data)
-	// })
-	
+
+	// when change state, auto close this dialog
+	$scope.$on('$stateChangeStart', e => {
+		$scope.close()
+	})
+
 	$scope.close = () => {
 		$mdDialog.hide()
 	}
-}])
+}))
